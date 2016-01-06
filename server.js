6076018f3bcdc3761d1b1bf0a1229916c983cda5
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-06 11:42:00 -0600

    Add disconnect event and message

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ var clientInfo = {};
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
+	socket.on('disconnect', function() {
+		var userData = clientInfo[socket.id];
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left the room',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function(req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
@@ -22,19 +35,19 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('message', function(message) {
-		var timestamp = moment().valueOf();	
+		var timestamp = moment().valueOf();
 		message.timestamp = timestamp;
-		console.log('Message received @ '  + moment.utc(message.timestamp).local().format('h:mm a') + 
-					': ' + message.text); // + message.timestamp.local().format('h:mm a') + ' '
+		console.log('Message received @ ' + moment.utc(message.timestamp).local().format('h:mm a') +
+			': ' + message.text); // + message.timestamp.local().format('h:mm a') + ' '
 		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	// timestamp property - Javascript timestamp (milliseconds)
-		socket.emit('message', {
-			name: 'System',
-			text: 'Welcome to the chat application',
-			timestamp: moment().valueOf()
-		});
+	socket.emit('message', {
+		name: 'System',
+		text: 'Welcome to the chat application',
+		timestamp: moment().valueOf()
+	});
 });
 
 http.listen(PORT, function() {
